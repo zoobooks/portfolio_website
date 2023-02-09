94e397263520d33b3ad3c220b09e3ed22e33eced
Zack Blaise <zblaise23@gmail.com>
2023-02-09 00:08:40 -0600

    webpack setup

---
+++ dist/bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nError: Cannot find module 'fs/promises'\\nRequire stack:\\n- /home/zb/projects/portfolio_website/node_modules/babel-loader/lib/cache.js\\n- /home/zb/projects/portfolio_website/node_modules/babel-loader/lib/index.js\\n- /home/zb/projects/portfolio_website/node_modules/loader-runner/lib/loadLoader.js\\n- /home/zb/projects/portfolio_website/node_modules/loader-runner/lib/LoaderRunner.js\\n- /home/zb/projects/portfolio_website/node_modules/webpack/lib/NormalModuleFactory.js\\n- /home/zb/projects/portfolio_website/node_modules/webpack/lib/Compiler.js\\n- /home/zb/projects/portfolio_website/node_modules/webpack/lib/webpack.js\\n- /home/zb/projects/portfolio_website/node_modules/webpack/lib/index.js\\n- /home/zb/projects/portfolio_website/node_modules/webpack-cli/lib/webpack-cli.js\\n- /home/zb/projects/portfolio_website/node_modules/webpack-cli/lib/bootstrap.js\\n- /home/zb/projects/portfolio_website/node_modules/webpack-cli/bin/cli.js\\n- /home/zb/projects/portfolio_website/node_modules/webpack/bin/webpack.js\\n    at Function.Module._resolveFilename (internal/modules/cjs/loader.js:815:15)\\n    at Function.Module._load (internal/modules/cjs/loader.js:667:27)\\n    at Module.require (internal/modules/cjs/loader.js:887:19)\\n    at require (internal/modules/cjs/helpers.js:74:18)\\n    at Object.<anonymous> (/home/zb/projects/portfolio_website/node_modules/babel-loader/lib/cache.js:24:5)\\n    at Module._compile (internal/modules/cjs/loader.js:999:30)\\n    at Object.Module._extensions..js (internal/modules/cjs/loader.js:1027:10)\\n    at Module.load (internal/modules/cjs/loader.js:863:32)\\n    at Function.Module._load (internal/modules/cjs/loader.js:708:14)\\n    at Module.require (internal/modules/cjs/loader.js:887:19)\");\n\n//# sourceURL=webpack://portfolio_website/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    })
+  ]
+};
